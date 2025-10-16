2e2afd485a1923ff693df3696f1b3b18920dde1e
ChrisEmi <ccamachop2000@alumno.ipn.mx>
2025-10-16 16:55:04 -0600

    refactor: eliminar código innecesario y mejorar la claridad en index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,10 +2,7 @@
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
 header('Content-Type: application/json');
-$metodo = $_SERVER['REQUEST_METHOD'];
-print_r($metodo);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../src/Core/database.php';
